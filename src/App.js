a66f753e4858e7734b04d8d31ae6155f78867efe
abhimanyu singh rathore <abhimanyu.rathore@loginradius.com>
2023-05-12 14:50:20 +0530

    update states

--- src/App.js
+++ src/App.js
@@ -1,29 +1,184 @@
 import React, { useState } from "react";
 import "./index.css";
+
 import Logo from "./logo.svg";
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-
+  const [searchTitle,setSearchTitle]= useState("")
+  const mockData={
+    "statsData": {
+        "total": 236
+    },
+    "chartData": {
+        "provider": [
+            {
+                "count": 207,
+                "key": "raas",
+                "data": [
+                    {
+                        "count": 5,
+                        "key": "2022-05-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 20,
+                        "key": "2022-06-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 24,
+                        "key": "2022-07-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 9,
+                        "key": "2022-08-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 2,
+                        "key": "2022-09-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 43,
+                        "key": "2022-10-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 3,
+                        "key": "2022-11-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 5,
+                        "key": "2022-12-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 8,
+                        "key": "2023-01-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 74,
+                        "key": "2023-02-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 6,
+                        "key": "2023-03-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 8,
+                        "key": "2023-04-01T00:00:00.000Z"
+                    }
+                ]
+            },
+            {
+                "count": 26,
+                "key": "google",
+                "data": [
+                    {
+                        "count": 2,
+                        "key": "2022-05-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 2,
+                        "key": "2022-06-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 4,
+                        "key": "2022-07-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 2,
+                        "key": "2022-08-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 0,
+                        "key": "2022-09-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 4,
+                        "key": "2022-10-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 3,
+                        "key": "2022-11-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 2,
+                        "key": "2022-12-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 1,
+                        "key": "2023-01-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 4,
+                        "key": "2023-02-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 1,
+                        "key": "2023-03-01T00:00:00.000Z"
+                    },
+                    {
+                        "count": 1,
+                        "key": "2023-04-01T00:00:00.000Z"
+                    }
+                ]
+            },
+            {
+                "count": 1,
+                "key": "saml_dev-aman",
+                "data": [
+                    {
+                        "count": 1,
+                        "key": "2022-07-01T00:00:00.000Z"
+                    }
+                ]
+            },
+            {
+                "count": 1,
+                "key": "saml_dev-neha100",
+                "data": [
+                    {
+                        "count": 1,
+                        "key": "2022-11-01T00:00:00.000Z"
+                    }
+                ]
+            },
+            {
+                "count": 1,
+                "key": "saml_dev-neha100e",
+                "data": [
+                    {
+                        "count": 1,
+                        "key": "2022-12-01T00:00:00.000Z"
+                    }
+                ]
+            }
+        ]
+    }
+}
   const handleInputChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
-  const handleSearch = () => {
-    const results = [
-      "Result 1",
-      "Result 2",
-      "Result 3",
-      "Result 4",
-      "Result 5",
-    ];
-
-    setSearchResults(results);
+  const handleSearch = (event) => {
+    const results = ["data1","data2"];
+    if(event.keyCode)
+    {
+        if(event.keyCode == 13){
+          setSearchResults(results);
+          setSearchTitle(event.currentTarget.value);
+          return true;
+        }
+        else {
+          return false;
+       }
+    
+    }else{
+      setSearchResults(results);
+      return true;
+    }    
   };
 
   return (
     <div id="page-container">
-      <div id="content-wrap">
+      {!searchResults.length && <div id="content-wrap">
         <div className="logo">
           <a href="/">
             <img src={Logo} />
@@ -36,15 +191,28 @@ function App() {
             improving productivity and enhancing customer experience.
           </p>
         </div>
+      </div>}
+      {!!searchResults.length &&<div id="content-wrap">
+        <div className="heading">
+        <h1>{searchTitle}</h1>
+          <p>
+              {searchResults && searchResults.map((result, index) => (
+                <p key={index}>{result}</p>
+              ))}
+          </p>
+        </div>
       </div>
+
+      }
       <div id="search_section">
-        <div class="search">
+        <div className="search">
           <input
             type="text"
-            class="searchTerm"
+            className="searchTerm"
             placeholder="Hint: how many user registered in last 10 days?"
+            onKeyDown={(e)=>handleSearch(e)}
           />
-          <button type="submit" class="searchButton" onClick={handleSearch}>
+          <button type="submit" className="searchButton" onClick={handleSearch} >
             <svg
               width="24"
               height="24"
@@ -69,7 +237,7 @@ function App() {
             </svg>
           </button>
         </div>
-        <div class="search_results">
+        {/* <div className="search_results">
           <p>
             {searchResults.length > 0}
             <ul className="results-list">
@@ -78,7 +246,7 @@ function App() {
               ))}
             </ul>
           </p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
