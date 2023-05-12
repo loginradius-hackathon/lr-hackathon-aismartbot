19e91cd99ee45866c228168cedb54c004a0529ca
abhimanyu singh rathore <abhimanyu.rathore@loginradius.com>
2023-05-12 21:17:18 +0530

    update local port fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.use(cors({
   origin: '*'
 }));
