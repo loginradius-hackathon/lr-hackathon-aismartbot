abdece0b3e5b6c4cfff436aa5fbdfacfe1f79b29
hemant manwani <hemant.manwani@loginradius.com>
2023-05-13 13:48:22 +0530

    Cache added.

--- routes.js
+++ routes.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
 let prompt =
   'Here is your Elastic Search Schema: {"properties":{"Age":{"type":"integer","normalizer":"lower_case_normalizer"},"CreatedDate":{"type":"date","normalizer":"lower_case_normalizer"},"EmailVerified":{"type":"boolean","normalizer":"lower_case_normalizer"},"LocalCountry":{"type":"text","normalizer":"lower_case_normalizer"},"Provider":{"type":"text","normalizer":"lower_case_normalizer"},"NoOfLogins":{"type":"int","normalizer":"lower_case_normalizer"},"BirthDate":{"type":"date","format":"MM-dd-yyyy"},"Gender":{"type":"keyword","normalizer":"lower_case_normalizer"},"user_agent":{"properties":{"device":{"properties":{"name":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}}}},"name":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"original":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"os":{"properties":{"full":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"name":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"version":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}}}}}}}} Take Gender Male as M and Female as F and rest a U';
 
@@ -10,87 +12,95 @@ const router = express.Router();
 
 //Post Method
 router.post("/query", async (req, res) => {
+  req.body.message=req.body.message.replace(/\s+/g, ' ').trim().toLowerCase();
+  var cache=!req.body.realtime
+  let key = `${process.env.DSL_ES_APPNAME}_${req.body.message}`
   console.log("Query-->>", req.body.message)
-  try {
-    let message = `Give ES Query for : ${req.body.message} with no explanation`;
-    const payload = {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: `${prompt}`,
-        },
-        {
-          role: "user",
-          content: message,
-        },
-      ],
-    };
-    axios
-      .post("https://api.openai.com/v1/chat/completions", payload, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_APIKEY}`,
-        },
-      })
-      .then(async (response) => {
-        if (
-          response.data != null &&
-          response.data.choices != null &&
-          response.data.choices[0].message != null &&
-          response.data.choices[0].message.content != null
-        ) {
-          var choiceData = response.data.choices[0].message.content
-            .replace(/[\r\n]/gm, "")
-            .replace(/\s/g, "");
-          var matchedString = choiceData.match(re);
-          if (matchedString && matchedString[0]) {
-            /* * AI is adding .keyword with text fields like Provider and LocalCountry 
-               *  data is not comming, so fixed that part by this logic  */
-            if (matchedString[0].includes(".keyword")) {
-              matchedString[0] = matchedString[0].replace(/.keyword/g, "")
-            }
-            console.log("es query ===> ", matchedString[0])
-            const response = await DslESAPI(JSON.parse(matchedString[0]));
-            console.log("DSL-ES Response", response.data)
-            const normalizeResp = {};
-            if (response.data.hits) {
-              normalizeResp["statsData"] = response.data.hits;
-            }
-            if (response.data.aggregations) {
-              const aggr = response.data.aggregations;
-              const charts = {};
-              for (const key in aggr) {
-                if (aggr[key].buckets) {
-                  charts[key] = normalizeESData(aggr[key].buckets);
-                  if (charts[key][0].data) {
-                    charts[key] = processComplexData(charts[key])
+  if (myCache.get(key) && cache) {
+    res.json(myCache.get(key))
+  } else {
+    try {
+      let message = `Give ES Query for : ${req.body.message} with no explanation`;
+      const payload = {
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: `${prompt}`,
+          },
+          {
+            role: "user",
+            content: message,
+          },
+        ],
+      };
+      axios
+        .post("https://api.openai.com/v1/chat/completions", payload, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.OPENAI_APIKEY}`,
+          },
+        })
+        .then(async (response) => {
+          if (
+            response.data != null &&
+            response.data.choices != null &&
+            response.data.choices[0].message != null &&
+            response.data.choices[0].message.content != null
+          ) {
+            var choiceData = response.data.choices[0].message.content
+              .replace(/[\r\n]/gm, "")
+              .replace(/\s/g, "");
+            var matchedString = choiceData.match(re);
+            if (matchedString && matchedString[0]) {
+              /* * AI is adding .keyword with text fields like Provider and LocalCountry 
+                 *  data is not comming, so fixed that part by this logic  */
+              if (matchedString[0].includes(".keyword")) {
+                matchedString[0] = matchedString[0].replace(/.keyword/g, "")
+              }
+              console.log("es query ===> ", matchedString[0])
+              const response = await DslESAPI(JSON.parse(matchedString[0]));
+              console.log("DSL-ES Response", response.data)
+              const normalizeResp = {};
+              if (response.data.hits) {
+                normalizeResp["statsData"] = response.data.hits;
+              }
+              if (response.data.aggregations) {
+                const aggr = response.data.aggregations;
+                const charts = {};
+                for (const key in aggr) {
+                  if (aggr[key].buckets) {
+                    charts[key] = normalizeESData(aggr[key].buckets);
+                    if (charts[key][0].data) {
+                      charts[key] = processComplexData(charts[key])
+                    }
+                  }
+                  if (aggr[key].value) {
+                    charts[key] = aggr[key].value
                   }
                 }
-                if (aggr[key].value) {
-                  charts[key] = aggr[key].value
-                }
+                normalizeResp["chartData"] = charts;
               }
-              normalizeResp["chartData"] = charts;
+              console.log("Response===>>", normalizeResp)
+              myCache.set(key, normalizeResp, 900)
+              res.json(normalizeResp);
+            } else {
+              res.statusCode = 403
+              res.json({ Message: "Invalid query" });
             }
-            console.log("Response===>>", normalizeResp)
-            res.json(normalizeResp);
           } else {
             res.statusCode = 403
             res.json({ Message: "Invalid query" });
           }
-        } else {
+        })
+        .catch((error) => {
           res.statusCode = 403
-          res.json({ Message: "Invalid query" });
-        }
-      })
-      .catch((error) => {
-        res.statusCode = 403
-        res.json({ Message: error});
-      });
-  } catch (ex) {
-    res.statusCode = 403
-    res.json({ Message: "Invalid query" });
+          res.json({ Message: error });
+        });
+    } catch (ex) {
+      res.statusCode = 403
+      res.json({ Message: "Invalid query" });
+    }
   }
 });
 
